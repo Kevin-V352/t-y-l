6c155e122b46e758ded9ea2664911e9c424940f2
Kevin Vega <kevinvega2070@gmail.com>
2022-11-19 00:45:40 -0300

    feat: added new page

--- components/pages/Home/index.tsx
+++ components/pages/Home/index.tsx
@@ -1,15 +1,81 @@
 import { FC } from 'react';
 
+import { useResponsive } from '@/hooks';
 import { MainLayout } from '@/layouts';
+import { CustomSlider, Slider, ProductSlider, AdvertisingBox } from '@/ui';
+
+import * as S from './styles';
+
+const sliderItems1 = [
+  { title: 'Licores' },
+  { title: 'Cerveza' },
+  { title: 'Cerveza' },
+  { title: 'Cerveza' },
+  { title: 'Cerveza' },
+  { title: 'Cerveza' },
+  { title: 'Cerveza' },
+  { title: 'Cerveza' }
+];
+
+const sliderItems2 = [
+  { title: 'Licores' },
+  { title: 'Cerveza' },
+  { title: 'Cerveza' }
+];
 
 const Home: FC = () => {
+
+  const currentResolution = useResponsive();
+  const isDesktop = currentResolution ? (currentResolution >= 1024) : false;
 
   return (
     <MainLayout
       title='TYL'
       desc='Venta de bebidas'
     >
-      <div>Home</div>
+      <section>
+        {
+          isDesktop
+            ? (
+              <CustomSlider
+                mode={1}
+                sliderItems={sliderItems1}
+              />
+              )
+            : <Slider />
+        }
+      </section>
+      <section>
+        <ProductSlider
+          title="NUEVO"
+          generalId="new_products"
+        />
+      </section>
+      <section>
+        <AdvertisingBox img=''/>
+      </section>
+      <section>
+        {
+          isDesktop
+            ? (
+              <CustomSlider
+                mode={2}
+                sliderItems={sliderItems2}
+              />
+              )
+            : <Slider />
+        }
+      </section>
+      <section>
+        <ProductSlider
+          title="POPULARES"
+          generalId="popular_products"
+          customStyles={S.popularSliderCustomStyles}
+        />
+      </section>
+      <section>
+        <AdvertisingBox img=''/>
+      </section>
     </MainLayout>
   );
 
