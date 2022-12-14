83441291ff341b18a03fa376fe5c93961dbe4816
Kevin Vega <kevinvega2070@gmail.com>
2022-12-14 01:50:50 -0300

    feat: changed way of obtaining cart data
    Replaced SSR with CSR for cart data collection through an endpoint.

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,39 +1,12 @@
-import type { NextPage, GetServerSideProps } from 'next';
+import type { NextPage } from 'next';
 
-import { dbProducts } from '@/database';
-import { ICartPageProps, ICartProduct } from '@/interfaces';
 import { Cart } from '@/pages';
-import { calculators } from '@/utils';
 
-const CartPage: NextPage<ICartPageProps> = ({ products }) => {
+const CartPage: NextPage = () => {
 
   return (
-    <Cart products={products} />
+    <Cart />
   );
-
-};
-
-export const getServerSideProps: GetServerSideProps = async ({ req: { cookies } }) => {
-
-  const defaultResponse = { props: { products: [] } };
-
-  const { cart }: { cart?: string } = cookies;
-
-  if (!cart) return defaultResponse;
-
-  const parsedCart: ICartProduct[] = JSON.parse(cart);
-
-  const ids = parsedCart.map((product) => product.id);
-
-  const [products] = await dbProducts.getProductsByIds(ids);
-
-  if (!products) return defaultResponse;
-
-  return {
-    props: {
-      products: calculators.revalidateStock(parsedCart, products)
-    }
-  };
 
 };
 
