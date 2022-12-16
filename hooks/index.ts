cdb62b182ac5bc3e39baa7ca4531c43b19a5bf94
Kevin Vega <kevinvega2070@gmail.com>
2022-12-16 01:34:53 -0300

    feat(useCart): unused hook deleted

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,4 +1,3 @@
-export { default as useCart } from './useCart';
 export { default as useFilter } from './useFilter';
 export { default as useQuantity } from './useQuantity';
 export { default as useResponsive } from './useResponsive';
