17c51fc9151bc2c393481ea0fa3fbbc88500554b
Kevin Vega <kevinvega2070@gmail.com>
2022-12-29 15:34:27 -0300

    feat(Button): props updated

--- components/ui/Button/types.ts
+++ components/ui/Button/types.ts
@@ -1,9 +1,12 @@
 import { ReactNode, ButtonHTMLAttributes } from 'react';
 
+import { CustomStyles } from '@/types';
+
 export interface IButtonWrapperProps {
   variant: 'primary' | 'outlined';
   fluid?: boolean;
   gridArea?: string;
+  customStyles?: CustomStyles;
 };
 
 export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, IButtonWrapperProps {
