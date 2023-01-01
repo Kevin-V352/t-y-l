3488016caafa04088c53c142d5132251634ebf53
Kevin Vega <kevinvega2070@gmail.com>
2022-12-31 22:08:52 -0300

    feat(Loader): new component added

---
+++ components/ui/Loader/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+import * as S from './styles';
+
+const Loader: FC = () => {
+
+  return (
+    <S.Container>
+      <S.CustomLoader />
+    </S.Container>
+  );
+
+};
+
+export default Loader;
