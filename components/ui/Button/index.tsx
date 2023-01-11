f62ec5dc7de84353bc2bdb8a2c7bd0f1940632ef
Kevin Vega <kevinvega2070@gmail.com>
2023-01-11 01:30:16 -0300

    fix(Button): component fixed

--- components/ui/Button/index.tsx
+++ components/ui/Button/index.tsx
@@ -1,17 +1,21 @@
-import { FC } from 'react';
+/* eslint-disable react/display-name */
+import { FC, forwardRef, ForwardedRef } from 'react';
 
 import * as S from './styles';
 import { IButtonProps } from './types';
 
-const Button: FC<IButtonProps> = (props) => {
+const Button: FC<IButtonProps> = forwardRef((props, ref) => {
 
   return (
-    <S.Wrapper {...props}>
+    <S.Wrapper
+      ref={ref as ForwardedRef<HTMLButtonElement>}
+      {...props}
+    >
       {props.text}
       {props.icon}
     </S.Wrapper>
   );
 
-};
+});
 
 export default Button;
