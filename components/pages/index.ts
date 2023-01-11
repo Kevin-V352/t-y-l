6c57010936ff9cafdbea3d7dd4414051baa71359
Kevin Vega <kevinvega2070@gmail.com>
2023-01-10 22:41:56 -0300

    feat(OpenChat): new page added

--- components/pages/index.ts
+++ components/pages/index.ts
@@ -1,6 +1,7 @@
 export { default as Address } from './Address';
 export { default as Cart } from './Cart';
 export { default as Home } from './Home';
+export { default as OpenChat } from './OpenChat';
 export { default as Product } from './Product';
 export { default as Search } from './Search';
 export { default as Summary } from './Summary';
