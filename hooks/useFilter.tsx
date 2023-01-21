29166acb6b1e8376a572d6065c2715544d74b635
Kevin Vega <kevinvega2070@gmail.com>
2023-01-20 22:38:48 -0300

    feat(useFilter): minor corrections

--- hooks/useFilter.tsx
+++ hooks/useFilter.tsx
@@ -2,9 +2,23 @@
 import { useRouter } from 'next/router';
 
 interface HookResponse {
+  queries:        IQueries;
+  currentQuery:   string | string[] | undefined;
+  currentPage:    number;
   changeCategory: (category: string, cb?: () => void) => void;
-  sortItems: (sort: TSortOptions) => void;
-  changePage: (page: number) => void;
+  sortItems:      (sort: TSortOptions) => void;
+  changePage:     (page: number) => void;
+};
+
+interface ILoadQueriesOptions {
+  removeSearch?:  boolean;
+  resetPage?:     boolean;
+};
+
+interface IQueries {
+  category?:  string;
+  sort?:      string;
+  page?:      string;
 };
 
 type TSortOptions = 'price_DESC' | 'price_ASC' | 'publishedAt_DESC';
@@ -14,23 +28,33 @@ const useFilter = (): HookResponse => {
 
   const router = useRouter();
 
-  const sortItems = (sort: TSortOptions): void => loadQueries('sort', sort, false);
-  const changePage = (page: number): void => loadQueries('page', page, false);
+  const currentQuery = router.query.query;
+  const currentPage = Number(router.query.page ?? 0);
+  const sortItems = (sort: TSortOptions): void => loadQueries('sort', sort);
+  const changePage = (page: number): void => loadQueries('page', page);
   const changeCategory = (category: string, cb?: () => void): void => {
 
-    loadQueries('category', category, true);
+    loadQueries('category', category, { removeSearch: true, resetPage: true });
     if (cb) cb();
 
   };
-  const loadQueries = (queryType: TQueryTypes, queryValue: string | number, removeSearch: boolean): void => {
+
+  const loadQueries = (
+    queryType: TQueryTypes,
+    queryValue: string | number,
+    options?: ILoadQueriesOptions
+  ): void => {
 
     const { query } = router;
 
     const newQueries = { ...query, [queryType]: queryValue };
+
+    if (options?.resetPage) newQueries.page = 0;
+
     const currentSearch = newQueries.query;
     delete newQueries.query;
 
-    const pathName = removeSearch ? '/search/0' : `/search/${currentSearch}`;
+    const pathName = options?.removeSearch ? '/search/0' : `/search/${currentSearch}`;
 
     router.push(
       {
@@ -42,6 +66,9 @@ const useFilter = (): HookResponse => {
   };
 
   return {
+    queries: router.query,
+    currentQuery,
+    currentPage,
     changeCategory,
     sortItems,
     changePage
